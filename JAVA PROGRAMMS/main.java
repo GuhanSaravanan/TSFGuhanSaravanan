b41b56f00ec8a1cc936a2dfbc93fb8d4d54029a9
GuhanSaravanan <19tuit023@skct.edu.in>
2021-08-06 19:41:14 +0530

    commit

--- JAVA PROGRAMMS/main.java
+++
@@ -1,6 +0,0 @@
-import java.io.*;
-class main{
-	public static void main(String []n){
-		System.out.println(n[0]+ "  Technologies  "+n[1]);
-	}
-}
--- JAVA PROGRAMMS/main2.java
+++
@@ -1,6 +0,0 @@
-import java.io.*;
-public class main2{
-	public static void main(String[]n2){
-		System.out.println(" Welcome "+n2[0]);
-	}
-}
